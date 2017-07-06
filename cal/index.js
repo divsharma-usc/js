24883c1d1259e0bf8959d6cefea0d89a240f033b
Divyansh Sharma <divyansh9595@gmail.com>
2017-07-06 20:50:02 +0530

    basic calendar added

---
+++ cal/index.js
@@ -0,0 +1,116 @@
+var calendar=function(){
+var date=new Date();
+var month=date.getMonth();
+var year=date.getFullYear();
+var nextyear=document.getElementById('nextyear');
+var prevyear=document.getElementById('prevyear');
+var nextmonth=document.getElementById('nextmonth');
+var prevmonth=document.getElementById('prevmonth');
+nextmonth.addEventListener('click',nextMonth);
+prevmonth.addEventListener('click',prevMonth);
+nextyear.addEventListener('click',nextYear);
+prevyear.addEventListener('click',prevYear);
+var days=['SUN','MON','TUES','WED','THU','FRI','SAT',];
+var months=['Jan','Feb','March','April','May','June','July','August','Sept','Oct','Nov','Dec'];
+var show_month_and_year=document.getElementById('monthandyear');
+var tablecontent=document.getElementById('tablecontent');
+var htmltable=tablecontent.innerHTML;
+show();
+function nextYear(){
+	year=year+1;
+	date.setFullYear(year);
+	show();
+
+}
+function prevYear(){
+	year=year-1;
+	date.setFullYear(year);
+	show();
+
+}
+function prevMonth(){
+	 if(date.getMonth()==0){
+	 	month=11;
+	 	year=year-1;
+	 	date.setFullYear(year);
+	 	date.setMonth(month);
+	 }
+	 else{
+	 	month=month-1;
+	 	date.setMonth(month);
+	 }
+	 show();
+}
+function nextMonth(){
+	 if(date.getMonth()==11){
+	 	month=0;
+	 	year=year+1;
+	 	date.setFullYear(year);
+	 	date.setMonth(month);
+	 }
+	 else{
+	 	month=month+1;
+	 	date.setMonth(month);
+	 }
+	 show();
+}
+function show(){
+	var obj=find();
+	var daysofmonth=obj.days;
+	var dayofmonth=obj.day;
+	var setdate=1;
+   show_month_and_year.innerHTML=months[month]+"-"+year;
+   htmltable='<table>'+'<tr>'
+   for(var i=0;i<7;i++){
+   	   htmltable=htmltable+'<th>'+" "+days[i]+" "+'</th>'
+   }
+    htmltable=htmltable+'</tr>'+'<tr>'
+   for(var i=0;i<7;i++){
+   	   if(i<dayofmonth)
+   	   htmltable=htmltable+'<th>'+" "+'</th>'
+   	   else{
+   	   	 htmltable=htmltable+'<th>'+setdate+'</th>'
+   	   	 setdate++;
+   	   }
+   }
+   htmltable=htmltable+'</tr>'
+   while(setdate<=daysofmonth){
+   	htmltable=htmltable+'<tr>';
+   	     for(var i=0;i<7;i++){
+   	     	htmltable=htmltable+'<th>'+setdate+'</th>'
+   	   	    setdate++;
+   	   	    if(setdate>daysofmonth)
+   	   	    	break;
+   	     }
+   	htmltable=htmltable+'</tr>'
+   }
+   htmltable=htmltable+'</table>'
+   tablecontent.innerHTML=htmltable;
+}
+function find(){
+	date.setDate(1);
+	var day=date.getDay();
+	var month=date.getMonth();
+	var days;
+	if(month==1){
+		if(year%4==0){
+         days=29;
+		} 
+		else{
+			days=28;
+		}
+	}
+	else if([0,2,4,6,7,9,11].includes(month)){
+		days=31;
+	}
+	else{
+		days=30;
+	}
+	var obj={
+		days:days,
+		day:day
+	}
+	return obj;
+}
+}
+calendar();
